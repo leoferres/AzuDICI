a5ab5ad763a2956284551b0695f0eaafa3e2e3ca
Juan Olate <jolate@gmail.com>
2012-02-01 19:07:02 -0300

    Improved thread safe vector ts_vec.h

--- src/ts_vec.h
+++ src/ts_vec.h
@@ -31,65 +31,81 @@
  *      ts_vec_resize(int,myVector,50);
  *  
  */
-
+#define __USE_UNIX98 1 
 #ifndef TS_VEC_H
 #define	TS_VEC_H
 
 #include <stdlib.h>
 #include <pthread.h>
+#include "kvec.h"
 
-pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
+pthread_rwlock_t lock_vector_rw_lock = PTHREAD_RWLOCK_INITIALIZER;
+kvec_t(pthread_rwlock_t) ts_vec_locks;
+bool init_flag = false;
+
 
 #define ts_vec_roundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
 
-#define ts_vec_t(type) struct { size_t n, m; type *a; }
+#define ts_vec_t(type) struct { size_t n, m; type *a; unsigned int id;}
 
-#define ts_vec_init(v) ((v).n = (v).m = 0, (v).a = 0)
+#define ts_vec_init(v) do{ \
+                          pthread_rwlock_wrlock(&lock_vector_rw_lock); \
+                          if(!init_flag){ \
+                                kv_init(ts_vec_locks); \
+                                init_flag = true; \
+                          } \
+                          (v).id = kv_size(ts_vec_locks); \
+                          ((v).n = (v).m = 0, (v).a = 0); \
+                          pthread_rwlock_t *lock = (pthread_rwlock_t *)malloc(sizeof(pthread_rwlock_t)); \
+                          pthread_rwlock_init(lock, NULL); \
+                          kv_push(pthread_rwlock_t,ts_vec_locks,lock[0]); \
+                          pthread_rwlock_unlock(&lock_vector_rw_lock); \
+                          } while(0)
 
 #define ts_vec_destroy(v) do{ \
-                        pthread_rwlock_wrlock(&rwlock); \
+                        pthread_rwlock_wrlock(&(kv_A(ts_vec_locks,(v).id))); \
                         free((v).a) \
-                        pthread_rwlock_unlock(&rwlock); \
+                        pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                         } while(0)
 
 #define ts_vec_ith(temp, v, i) do{ \
-                               pthread_rwlock_rdlock(&rwlock); \
+                               pthread_rwlock_rdlock(&(kv_A(ts_vec_locks,(v).id))); \
                                temp= ((v).a[(i)]); \
-                               pthread_rwlock_unlock(&rwlock); \
+                               pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                } while(0)
 
 #define ts_vec_pop(temp, v) do{ \
-                               pthread_rwlock_wrlock(&rwlock); \
+                               pthread_rwlock_wrlock(&(kv_A(ts_vec_locks,(v).id))); \
                                temp= ((v).a[--(v).n]); \
-                               pthread_rwlock_unlock(&rwlock); \
+                               pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                } while(0)
 
 #define ts_vec_size(temp, v) do{ \
-                               pthread_rwlock_rdlock(&rwlock); \
+                               pthread_rwlock_rdlock(&(kv_A(ts_vec_locks,(v).id))); \
                                temp= ((v).n); \
-                               pthread_rwlock_unlock(&rwlock); \
+                               pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                } while(0)
 
 #define ts_vec_max(temp, v) do{ \
-                        pthread_rwlock_rdlock(&rwlock); \
+                        pthread_rwlock_rdlock(&(kv_A(ts_vec_locks,(v).id))); \
                         temp= ((v).m); \
-                        pthread_rwlock_unlock(&rwlock); \
+                        pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                         } while(0)
 
 #define ts_vec_resize(type, v, s) do{ \
-                                        pthread_rwlock_wrlock(&rwlock); \
+                                        pthread_rwlock_wrlock(&(kv_A(ts_vec_locks,(v).id))); \
                                         ((v).m = (s), (v).a = (type*)realloc((v).a, sizeof(type) * (v).m)); \
-                                        pthread_rwlock_unlock(&rwlock); \
+                                        pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                     } while(0) 
 
 #define ts_vec_push_back(type, v, x) do { \
-                                        pthread_rwlock_wrlock(&rwlock); \
+                                        pthread_rwlock_wrlock(&(kv_A(ts_vec_locks,(v).id))); \
                                         if ((v).n == (v).m) { \
                                                 (v).m = (v).m? (v).m<<1 : 2; \
                                                 (v).a = (type*)realloc((v).a, sizeof(type) * (v).m); \
                                         } \
                                         (v).a[(v).n++] = (x); \
-                                        pthread_rwlock_unlock(&rwlock); \
+                                        pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                    } while (0)
 
 #define ts_vec_pushp(type, v) (((v).n == (v).m)? \
@@ -98,13 +114,13 @@ pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
 						   : 0), ((v).a + ((v).n++))
 
 #define ts_vec_set_ith(type, v, i, temp)              do{ \
-                                                pthread_rwlock_wrlock(&rwlock); \
+                                                pthread_rwlock_wrlock(&(kv_A(ts_vec_locks,(v).id))); \
                                                 ((v).m <= (size_t)(i)? \
 						  ((v).m = (v).n = (i) + 1, ts_vec_roundup32((v).m), \
 						   (v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0) \
 						  : (v).n <= (size_t)(i)? (v).n = (i) \
 						  : 0), (v).a[(i)] = temp; \
-                                                pthread_rwlock_unlock(&rwlock); \
+                                                pthread_rwlock_unlock(&(kv_A(ts_vec_locks,(v).id))); \
                                               } while(0)
 
 #endif	/* TS_VEC_H */
