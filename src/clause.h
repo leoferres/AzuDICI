7fd21535c10f1ffe88421c269c893cd67097c0e5
Juan Olate <jolate@gmail.com>
2012-02-01 13:30:16 -0300

    Fixed some typos in clause.h

--- src/clause.h
+++ src/clause.h
@@ -7,21 +7,21 @@
 #include "ts_vec.h"
 
 typedef struct _naryclause {
-  ts_vec(bool)  flags; //thread safe vector of literals
-  kvec(Literal) lits; 
+  ts_vec_t(bool)  flags; //thread safe vector of literals
+  kvec_t(Literal) lits; 
   //unsigned int size;
   bool is_original; /*yes=input clause, no=lemma*/
   char padding[3];  /*half a cacheline per n-ary clause*/ //RECALCULATE THIS DEPENDING ON ts_vec size
 } NClause; 
 
 typedef struct _teryclause {
-  ts_vec(bool) flags; //thread safe vector of literals
+  ts_vec_t(bool) flags; //thread safe vector of literals
   Literal lits[3];
   char padding[4]; /*half a cacheline per ternary clause*/ //RECALCULATE THIS DEPENDING ON ts_vec size
 } TClause; 
 
 typedef struct _clause {
-  kvec(Literal) lits;
+  kvec_t(Literal) lits;
   unsigned int size;
 } Clause; /*Can be unit, binary, ternary or nary*/
 
